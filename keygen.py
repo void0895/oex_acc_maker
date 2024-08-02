6997116479bbd4c49776b90bf5d12d287e035c24
void0895 <164507313+void0895@users.noreply.github.com>
2024-08-02 12:18:16 +0530

    added arg handling
    k

--- keygen.py
+++ keygen.py
@@ -1,13 +1,27 @@
 from os import urandom
 from eth_keys import keys
+import argparse
 
 
-with open("keys.txt", "w") as f:
-    for _ in range(100):
-        b = urandom(32)
-        private_key = keys.PrivateKey(b)
-        f.write(str(private_key))
-        eth_address = private_key.public_key.to_checksum_address()
-        f.write("   ")
-        f.write(str(eth_address))
-        f.write("\n")
+def gen(filename, num):
+    with open(filename, "w") as f:
+        for _ in range(num):
+            b = urandom(32)
+            private_key = keys.PrivateKey(b)
+            f.write(str(private_key))
+            eth_address = private_key.public_key.to_checksum_address()
+            f.write("   ")
+            f.write(str(eth_address))
+            f.write("\n")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='generate eth priv key and eth address\nUsage : python keygen.py filename num_of_keys\npython keygen.py keys.txt 500')
+    parser.add_argument('filename', type=str, help="filename")
+    parser.add_argument("num", type=int, help="number of keys to generate")
+    args = parser.parse_args()
+    gen(args.filename, args.num)
+
+
+main()
